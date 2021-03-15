59097b620ee64cd5cdd13f39e892927a511344ba
honus <63273720+0honus0@users.noreply.github.com>
2021-03-15 19:25:54 +0800

    Update music.js

--- music.js
+++ music.js
@@ -1,1 +1,2 @@
 name="test" artist="rainymood" url="https://sharelist.honus.top/honus/music/A%20Fine%20Frenzy%20-%20Almost%20Lover.MP3"
+name="test1" artist="rainymood1" url="https://sharelist.honus.top/honus/music/Against%20the%20Current%20-%20Legends%20Never%20Die.MP3"
