7d3dc2b22d47bc00e944cc3eee827215962e13c3
honus <63273720+0honus0@users.noreply.github.com>
2021-03-15 21:31:09 +0800

    Update music.js

--- music.js
+++ music.js
@@ -1,2 +1,11 @@
-audio:[{name="test" artist="rainymood" url="https://sharelist.honus.top/honus/music/A%20Fine%20Frenzy%20-%20Almost%20Lover.MP3"},
-       {name="test1" artist="rainymood1" url="https://sharelist.honus.top/honus/music/Against%20the%20Current%20-%20Legends%20Never%20Die.MP3"}]
+audio: [{
+        name: 'name1',
+        artist: 'artist1',
+        url: 'https://sharelist.honus.top/honus/music/A%20Fine%20Frenzy%20-%20Almost%20Lover.MP3',
+        cover: 'cover1.jpg'
+    }, {
+        name: 'name2',
+        artist: 'artist2',
+        url: 'https://sharelist.honus.top/honus/music/Against%20the%20Current%20-%20Legends%20Never%20Die.MP3',
+        cover: 'cover2.jpg'
+    }]
