8b04c5085474a7d1af910b1b68f535593a225232
honus <63273720+0honus0@users.noreply.github.com>
2021-03-15 20:21:24 +0800

    Update music.js

--- music.js
+++ music.js
@@ -1,14 +1,1 @@
-<div class="aplayer"
-    data-name="广东珠江经济电台"
-    data-artist="FM97.4"
-    data-url="http://lhttp.qingting.fm/live/1259/64k.mp3"
-    data-cover="http://pic.qingting.fm/2015/0209/20150209212831195.jpg!200"
-    data-fixed="true">
-</div>
-<div class="aplayer"
-    data-name="广东珠江经济电台"
-    data-artist="FM97.4"
-    data-url="http://lhttp.qingting.fm/live/1259/64k.mp3"
-    data-cover="http://pic.qingting.fm/2015/0209/20150209212831195.jpg!200"
-    data-fixed="true">
-</div>
+name="test" artist="rainymood" url="https://sharelist.honus.top/honus/music/A%20Fine%20Frenzy%20-%20Almost%20Lover.MP3"
